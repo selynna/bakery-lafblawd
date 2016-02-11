cf3c9f9b8f1b75550d1f39b7fa28311f018427fd
Justin Yu <justin.v.yu@gmail.com>
2016-02-11 11:47:48 -0800

    Add contact

--- js/index.js
+++ js/index.js
@@ -6,3 +6,30 @@ $('.menu-wrapper').slick({
   slidesToShow: 1,
   adaptiveHeight: true
 });
+
+// $('.menu-wrapper').slick({
+//   centerMode: true,
+//   centerPadding: '60px',
+//   slidesToShow: 1,
+//   infinite: false,
+//   responsive: [
+//     {
+//       breakpoint: 768,
+//       settings: {
+//         arrows: false,
+//         centerMode: true,
+//         centerPadding: '40px',
+//         slidesToShow: 3
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         arrows: false,
+//         centerMode: true,
+//         centerPadding: '40px',
+//         slidesToShow: 1
+//       }
+//     }
+//   ]
+// });
